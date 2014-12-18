2542e488e60c04a0fc58645ac6628d7a4b82a422
Ionică Bizău <bizauionica@yahoo.com>
2014-12-18 18:46:36 +0200

    Use artists module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Request = require("request")
   , Cheerio = require("cheerio")
   , Config = require("./config")
   , Auth = require("./auth")
+  , Artists = require("./artists")
   , Fs = require("fs")
   ;
 
@@ -10,4 +11,8 @@ console.log("Authenticating...");
 Auth.getCookie(function (err, cookie) {
    if (err) throw err;
    console.log("Got the cookie: " + cookie);
+   Config.cookie = cookie;
+   Artists.all(function (err, allArtists) {
+       console.log("Fetched " + allArtists.length + " artists.");
+   });
 });
