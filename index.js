835c2b08319e3919e0956f0cf315ce83e053d5e6
Ionică Bizău <bizauionica@yahoo.com>
2014-12-18 21:41:08 +0200

    Call Artworks module

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ var Request = require("request")
   , Config = require("./config")
   , Auth = require("./auth")
   , Artists = require("./artists")
+  , Artworks = require("./artworks")
   , Fs = require("fs")
   ;
 
 console.log("Authenticating...");
 Auth.getCookie(function (err, cookie) {
-   if (err) throw err;
-   console.log("Got the cookie: " + cookie);
-   Config.cookie = cookie;
-   Artists.all(function (err, allArtists) {
-       console.log("Fetched " + allArtists.length + " artists.");
-   });
+    if (err) throw err;
+    console.log("Got the cookie: " + cookie);
+    Config.cookie = cookie;
+    Artists.all(function (err, allArtists) {
+        console.log("Fetched " + allArtists.length + " artists.");
+        function handleUsr(u) {
+            Artworks.downloadFromArtist(u, function (err, artws) {
+                if (err) { console.log("Something went wrong when downloading artworks from user: " + u.display_name, err); }
+                else {
+                    console.log("Fetched all artworks from " + u.display_name + " but images are still downloading.");
+                }
+            });
+        }
+        for (var i = 0; i < allArtists.length; ++i) {
+            handleUsr(allArtists[i]);
+        }
+    });
 });
